3c6dc8c898bfe296bc98bc7fb02d2a237eab4996
Enzo <enzo.rinaldi.2009@gmail.com>
2025-12-19 15:02:45 +0100

    dynamic display without reloading and addition of a refresh timer

--- app/src/main/python/test.py
+++ app/src/main/python/test.py
@@ -1,14 +1,21 @@
-import pronotepy
-from pronotepy.ent import ile_de_france
+import datetime
 
-# Connexion
+import pronotepy
+from pronotepy.ent import monlycee
+
 try:
     client = pronotepy.Client("https://0782549x.index-education.net/pronote/eleve.html",
                               username="alexis.josseaume",
                               password="@Pronote2025",
-                              ent=ile_de_france)
+                              ent=monlycee)
     if client.logged_in:
         print("connexion réussie")
+    date_from = datetime.date(2025, 12, 19)
+    for i in client.get_recipients():
+        print(i.name)
+    infos = client.info.class_name
+    print(infos)
+
 
 except Exception as e:
     print(e)
